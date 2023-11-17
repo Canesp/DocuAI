9aa6be6353cbd23d858d07b715fcf9128551350c
Canesp <philip.decanesie@gmail.com>
2023-11-17 22:52:58 +0100

    moved files out of src and del src folder

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(),
     entry_points={
         'console_scripts': [
-            'DOCUAI = DOCUAI.src.main:main',
+            'DOCUAI = DOCUAI.main:main',
         ],
     },
 )
